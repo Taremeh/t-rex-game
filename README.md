fd6c99b737dea6bea69742569bcb19010accc0f5
Richard Liu <liuzhenglaichn@gmail.com>
2017-12-25 22:56:30 -0800

    0.0.1: t-rex with blinking eye

---
+++ README.md
@@ -0,0 +1,9 @@
+# T-Rex
+
+Trying to replicate the T-Rex game of Google.
+
+# Updates
+
+Version | Update
+---|---
+0.0.1 | T-Rex with blinking eye
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+    {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+    },
+    {
+        test: /\.png$/,
+        use: [
+         'file-loader'
+        ]
+    }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: 'dist'
+  }
+};
